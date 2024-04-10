85652b0f39573e2e2dfd6e727cf7e2b485e2c29d
Prithika <titlidatt1000@gmail.com>
2024-04-10 16:54:58 +0530

    model2 added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
-
+model2 = pickle.load(open('model2.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -22,14 +22,20 @@ def explore():
         request.form.get('Avg Wave Energy Flux'),
         request.form.get('Avg Wave Energy Power')
     ]
-  
+    
+     # Convert input values to numeric type
+    features = [float(value) if value is not None else 0.0 for value in features]  # Convert to float, replace None with default value
+    
     # Make predictions
     prediction = model.predict([features])
+    typepred = model2.predict([features])
     print([features])
     print(prediction)
-    return render_template('explore.html', prediction_text=prediction)
+    print(typepred)
+    return render_template('explore.html', prediction_text=prediction, prediction_type=typepred)
 
 
+ #, prediction_type=typepred
 if __name__=='__main__':
     app.run(debug=True)
     
